84e4c76098ae4f0fa15cc971ccc1eeb0cb5ca258
HariharanIT <ghariharanit@gmail.com>
2022-08-01 12:14:28 +0530

    Added sample components for testing

---
+++ appbuilderfpe/test-fpe/custom-components/CustomParticipantPanel.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import {ParticipantsView} from 'fpe-api';
+const CustomParticipantPanel = () => {
+  return <ParticipantsView />;
+};
+export {CustomParticipantPanel};
--- appbuilderfpe/test-fpe/index.tsx
+++ appbuilderfpe/test-fpe/index.tsx
@@ -1,27 +1,43 @@
 import {installFPE} from 'fpe-api/install';
-/* Custom imports
-import NewCustomLayout from './pages/NewCustomLayout';
-import ChatBubble from './components/ChatBubble';
-*/
+import CustomBottomBar from './custom-components/CustomBottomBar';
+import TopPinnedVideo from './custom-layout/TopPinnedLayout';
+import {CustomWrapperProvider} from './custom-wrapper/CustomWrapper';
+import {CustomMaxVideoView} from './custom-components/CustomMaxVideoView';
+import {CustomParticipantPanel} from './custom-components/CustomParticipantPanel';
 
 const userCustomization = installFPE({
-  // components: {
-  //   videoCall: {
-  // # Pass a function that returns an array of layout objects to override available layouts
-  //     customLayout: (defaultLayouts) => {
-  //       return [...defaultLayouts, {
-  //           label: 'Vertical',
-  //           name: 'vertical_pinned',
-  //           iconName: 'screenshare'
-  //           component: NewCustomLayout;
-  //         }];
-  //     },
-  // # Pass custom components here to override default components
-  //     componentOverrides: {
-  //        chatBubble: ChatBubble,
-  //     },
-  //   },
-  // },
+  appRoot: CustomWrapperProvider,
+  components: {
+    videoCall: {
+      participantsPanel: CustomParticipantPanel,
+      bottomBar: CustomBottomBar,
+      customContent: {
+        rtc: CustomMaxVideoView,
+      },
+      customLayout(defaultLayouts) {
+        return [
+          ...defaultLayouts,
+          {
+            component: TopPinnedVideo,
+            label: 'Top Pinned Layout',
+            name: 'TopPinnedLayout',
+            iconName: 'clipboard',
+          },
+        ];
+      },
+    },
+  },
+  i18n: [
+    {
+      locale: 'en-us',
+      label: 'English',
+      data: {
+        joinRoomButton: 'Join Room',
+        meetingNameInputPlaceholder: 'Room name',
+        pstnUserLabel: 'PSTN USER',
+      },
+    },
+  ],
 });
 
 export default userCustomization;
